de4bf0f98b8037f515c30d9d9fbee00d24957f55
gosth <danxuedexing@qq.com>
2017-06-20 00:33:12 +0800

    8.1 Advertising.py 2th

--- Regression/8.1.Advertising.py
+++ Regression/8.1.Advertising.py
@@ -16,7 +16,43 @@ if __name__ == "__main__":
     print x
     print y
     plt.plot(data['TV'], y, 'ro', label = 'TV')
+    #plt.plot(data['Radio'], y, 'g^', label = 'Radio')
+    #plt.plot(data['Newspaper'], y, 'mv', label = 'Newspaper')
     plt.legend(loc= 'lower right')
     plt.grid()
     plt.show()
     
+    plt.figure(figsize=(9,12))
+    plt.subplot(311)
+    plt.plot(data['TV'], y, 'ro')
+    plt.title('TV')
+    plt.grid()
+    plt.subplot(312)
+    plt.plot(data['Radio'], y, 'g^')
+    plt.title('Radio')
+    plt.grid()
+    plt.subplot(313)
+    plt.plot(data['Newspaper'], y, 'b*')
+    plt.title('Newspaper')
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
+    linreg = LinearRegression()
+    model = linreg.fit(x_train, y_train)
+    print model
+    print linreg.coef_
+    print linreg.intercept_
+
+    y_hat = linreg.predict(np.array(x_test))
+    mse = np.average((y_hat - np.array(y_test)) ** 2)
+    rmse = np.sqrt(mse)
+    print mse,rmse
+
+    t = np.arange(len(x_test))
+    plt.plot(t, y_test, 'r-', linewidth = 2, label = 'Test')
+    plt.plot(t, y_hat, 'g-', linewidth = 2, label = 'Predict')
+    plt.legend( loc = 'upper right')
+    plt.grid()
+    plt.show()
