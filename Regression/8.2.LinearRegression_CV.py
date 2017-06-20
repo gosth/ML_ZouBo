74dfa378f24084a358e7ecb3b1170980dd0c865e
gosth <danxuedexing@qq.com>
2017-06-20 16:13:42 +0800

    8.2.LinearRegression_CV.py 1st

---
+++ Regression/8.2.LinearRegression_CV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso, Ridge
+from sklearn.model_selection import GridSearchCV
+
+if __name__ == "__main__":
+    data = pd.read_csv('8.Advertising.csv')
+    x = data[['TV', 'Radio', 'Newspaper']]
+    y = data['Sales']
+    print x
+    print y
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
+    model = Lasso()
