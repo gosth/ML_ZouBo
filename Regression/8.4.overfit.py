9eaab180470d87af05c37a5a9ca624850ee70412
gosth <danxuedexing@qq.com>
2017-07-05 21:56:58 +0800

    8.4.overfit.py 1st

---
+++ Regression/8.4.overfit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import numpy as np
+from sklearn.linear_model import LinearRegression, RidgeCV
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+import matplotlib as mpl
+
+
+if __name__ == "__main__":
+    np.random.seed(0)
+    N = 9
+    x = np.linspace(0, 6, N) + np.random.randn(N)
+    x = np.sort(x)
+    y = x ** 2 - 4 * x -3 + np.random.randn(N)
+
