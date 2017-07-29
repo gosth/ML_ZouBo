cbae7a323154e8f432c75a615f9fc38456741084
gosth <danxuedexing@qq.com>
2017-07-29 21:52:29 +0800

    10.1.Iris_DecisionTree.py 2nd

--- RandomForest/10.1Iris_DecisionTree.py
+++ RandomForest/10.1Iris_DecisionTree.py
@@ -11,3 +11,10 @@ from sklearn.pipeline import Pipeline
 def iris_type(s):
     it = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
     return it[s]
+
+iris_feature = u'花萼长度', u'花萼宽度', u'花瓣长度', u'花瓣宽度'
+
+if __name__ == "__main__":
+    mpl.rcParams['font.sans-serif'] = [u'SimHei']
+    mpl.rcParams['axes.unicode_minus'] = False
+    path = '..\\Regression\\8.iris.data'
